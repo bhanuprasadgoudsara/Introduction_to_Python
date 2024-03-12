83196512db9a89c0cf45429f51c4dbc24cb67026
Sara Bhanu Prasad Goud <154022197+bhanuprasadgoudsara@users.noreply.github.com>
2024-03-12 11:24:06 +0530

    Add files via upload

---
+++ Employees/ManageEmployees.py
@@ -0,0 +1,3 @@
+employer ="Bhanu Prasad"
+def add(no_of_employees):
+    print(no_of_employees,"employees added to the company")
---
+++ Flights/Manage.py
@@ -0,0 +1,3 @@
+airline="Smart Airlines"
+def add(no_of_flights):
+    print(no_of_flights,"flights added to the fleet")
